00038e6a65918232f9d47caad2ffceb44fae8b37
inerjanuer <inerbernate@gmail.com>
2024-08-21 15:28:02 -0500

    add reestructure

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,6 +21,8 @@ return new class extends Migration
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
+            $table->string('type_identification');
+            $table->string('identification');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
